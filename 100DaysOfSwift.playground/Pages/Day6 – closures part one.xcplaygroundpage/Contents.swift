4c438a189dcf3dd95ba3dc2e29d0fe9b5df03e12
Jess Lê <jessicale90@gmail.com>
2023-08-14 16:01:01 -0600

    Day 6 done

---
+++ 100DaysOfSwift.playground/Pages/Day6 – closures part one.xcplaygroundpage/Contents.swift
@@ -0,0 +1,71 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//https://www.hackingwithswift.com/100/6
+
+//: [Next](@next)
+// You can create a function and assign it to a variable
+// call that function using that variable
+// and even pass that function into other functions as parameters.
+// Functions used in this way are called closures, and although they work like functions they are written a little differently.
+
+
+// WHY: to store functionality – to be able to say “here’s some work I want you to do at some point, but not necessarily now.”
+let driving = {
+    print("I'm driving in my car")
+}
+
+driving()
+
+
+let drivingTo = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+drivingTo("London")
+
+
+let drivingWithReturn = { (place: String) in
+    return "I am driving to \(place) in my car"
+}
+let message = drivingWithReturn("London")
+print(message)
+
+
+let payment = { () -> Bool in
+    print("Paying an anonyous person...")
+    return true
+}
+if payment() == true {
+    print("Completed payment")
+}
+
+
+// MARK: - Closures as Parameters
+func travel(action: () -> Void) {
+    print("I'm getting ready to go.")
+    action()
+    print("I arrived!")
+}
+travel(action: driving)
+
+func travelTo(action: (String) -> Void) {
+    print("I'm getting ready to go.")
+    action("London")
+    print("I arrived!")
+}
+
+// allows you to pass in closure with parameter
+travelTo(action: { (place: String) in
+    print("I am traveling to \(place) in my car")
+})
+
+// MARK: - Trailing Closure Syntax
+
+travel() {
+    print("I'm driving in my car")
+}
+
+travel {
+    print("I'm driving in my car")
+}
